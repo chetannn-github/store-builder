39332f2715eaa0213d45471d6bdc76a27c67a1c9
chetannn-github <chetan.rajawat25@gmail.com>
2026-02-07 11:31:33 +0530

    feat - medusa store backend and admin panel

--- server/src/utils/commands.js
+++ server/src/utils/commands.js
@@ -5,7 +5,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const getStoreCreationCommand = (namespace, storeType, domain) => {
-    const chartPath = path.resolve(__dirname, '../charts/storefront');
+    const chartFolder = (storeType === "medusa") ? "medusa" : "storefront"; 
+
+    const chartPath = path.resolve(__dirname, `../charts/${chartFolder}`);
     const releaseName = namespace;
 
     const storeCreationCommand = `helm install ${releaseName} ${chartPath} \
