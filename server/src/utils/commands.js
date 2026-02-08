332ba7c63e37a872f6c67ee2cb5974a07deae4fd
chetannn-github <chetan.rajawat25@gmail.com>
2026-02-08 19:57:13 +0530

    fix - prod deployment command

--- server/src/utils/commands.js
+++ server/src/utils/commands.js
@@ -10,15 +10,19 @@ export const getStoreCreationCommand = (namespace, storeType, domain, email, pas
     const chartFolder = (storeType === "medusa") ? "medusa" : "storefront"; 
     const chartPath = path.resolve(__dirname, `../charts/${chartFolder}`);
     const releaseName = namespace;
-
+ const isProduction = process.env.NODE_ENV === 'production';
+    const ingressClass = isProduction ? "traefik" : "nginx";
     const adminEmail = email || "admin@default.com";
     const adminPass = password || "secret123";
 
     let storeCreationCommand = `helm install ${releaseName} ${chartPath} \
       --namespace ${namespace} \
       --create-namespace \
+      --set ingress.className="${ingressClass}" \
       --set ingress.host=${domain} \
       --set store.type=${storeType} \
+       --set wordpress.siteUrl="http://${domain}" \
+    --set service.type="ClusterIP" \
       --values ${chartPath}/values-local.yaml \
       --wait`;
 
