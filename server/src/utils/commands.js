508e956eabdd5cee3ad5ff16c077f8998d264200
chetannn-github <chetan.rajawat25@gmail.com>
2026-02-08 19:10:24 +0530

    fix - wordpress prod command

--- server/src/utils/commands.js
+++ server/src/utils/commands.js
@@ -5,22 +5,32 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+
+
 export const getStoreCreationCommand = (namespace, storeType, domain, email, password) => {
-    
+    const isProduction = process.env.NODE_ENV === 'production';
+    const ingressClass = isProduction ? "traefik" : "nginx"; 
     const chartFolder = (storeType === "medusa") ? "medusa" : "storefront"; 
     const chartPath = path.resolve(__dirname, `../charts/${chartFolder}`);
     const releaseName = namespace;
+    const baseDomain = process.env.BASE_DOMAIN || "localhost";
+    const domain = `store-${storeName}.${baseDomain}`;
 
+    
     const adminEmail = email || "admin@default.com";
     const adminPass = password || "secret123";
 
-    let storeCreationCommand = `helm install ${releaseName} ${chartPath} \
-      --namespace ${namespace} \
-      --create-namespace \
-      --set ingress.host=${domain} \
-      --set store.type=${storeType} \
-      --values ${chartPath}/values-local.yaml \
-      --wait`;
+   
+
+    const storeCreationCommand = `helm install ${releaseName} ${chartPath} \
+    --namespace ${namespace} \
+    --create-namespace \
+    --set ingress.className="${ingressClass}" \
+    --set ingress.host="${domain}" \
+    --set wordpress.siteUrl="http://${domain}" \
+    --set service.type="ClusterIP" \
+    --set store.type=${storeType} \
+    --wait`;
 
     if (storeType === "medusa") {
       storeCreationCommand += ` \
