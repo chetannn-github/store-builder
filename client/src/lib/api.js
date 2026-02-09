d3d34ef5ac66fb8f344308d085f26933dbb8a815
chetannn-github <chetan.rajawat25@gmail.com>
2026-02-09 17:42:17 +0530

    fix - cors issue

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -16,28 +16,28 @@ async function handleRes(res) {
 async function get(path, token) {
   const headers = {}
   if (token) headers['Authorization'] = 'Bearer ' + token
-  const res = await fetch(BASE_URL + path, { headers , credentials: "include",})
+  const res = await fetch(BASE_URL + path, { headers})
   return await handleRes(res)
 }
 
 async function post(path, body, token) {
   const headers = { 'Content-Type': 'application/json' }
   if (token) headers['Authorization'] = 'Bearer ' + token
-  const res = await fetch(BASE_URL + path, { method: 'POST', headers, body: JSON.stringify(body) , credentials: "include",})
+  const res = await fetch(BASE_URL + path, { method: 'POST', headers, body: JSON.stringify(body)})
   return await handleRes(res)
 }
 
 async function put(path, body, token) {
   const headers = { 'Content-Type': 'application/json' }
   if (token) headers['Authorization'] = 'Bearer ' + token
-  const res = await fetch(BASE_URL + path, { method: 'PUT', headers, body: JSON.stringify(body), credentials: "include", })
+  const res = await fetch(BASE_URL + path, { method: 'PUT', headers, body: JSON.stringify(body) })
   return await handleRes(res)
 }
 
 async function del(path ,body, token) {
   const headers = { 'Content-Type': 'application/json' }
   if (token) headers['Authorization'] = 'Bearer ' + token
-  const res = await fetch(BASE_URL + path, { method: 'DELETE', headers, body : JSON.stringify(body), credentials: "include",})
+  const res = await fetch(BASE_URL + path, { method: 'DELETE', headers, body : JSON.stringify(body)})
   return await handleRes(res)
 }
 
@@ -45,7 +45,7 @@ async function del(path ,body, token) {
 async function patch(path, token) {
   const headers = {}
   if (token) headers['Authorization'] = 'Bearer ' + token
-  const res = await fetch(BASE_URL + path, { method: 'PATCH', headers , credentials: "include",})
+  const res = await fetch(BASE_URL + path, { method: 'PATCH', headers})
   return await handleRes(res)
 }
 
