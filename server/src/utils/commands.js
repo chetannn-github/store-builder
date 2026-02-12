842864d21975b9c77ee9f26cac612d6f12f7c6c6
chetannn-github <chetan.rajawat25@gmail.com>
2026-02-12 10:17:19 +0530

    fix - change wordpress version

--- server/src/utils/commands.js
+++ server/src/utils/commands.js
@@ -26,7 +26,7 @@ export const getStoreDeletionCommand = (namespace) => {
 
 const getWoocommerceStoreCommand = (email, password, namespace, chartPath,storeType,domain, ingressClass ) => {
   const myCustomImage = "chetannn/custom-store-builder"; 
-  const imageTag = "ssl";
+  const imageTag = "ssll";
 
   const devPostStartScript = `
     echo "Waiting for WordPress to initialize...";
