c68a51698031e9e913020dd740a55ef495e49050
chetannn-github <chetan.rajawat25@gmail.com>
2026-02-12 10:39:39 +0530

    feat - made created to https

--- woocommerce/mu-plugins/iframe-fix.php
+++ woocommerce/mu-plugins/iframe-fix.php
@@ -5,10 +5,7 @@ Description: Removes X-Frame-Options to allow iframe embedding.
 */
 
 add_action('send_headers', function() {
-    // 1. Remove the blocking header
     header_remove("X-Frame-Options");
-    
-    // 2. Add the allowing header
     header("Content-Security-Policy: frame-ancestors *");
 });
 ?>
