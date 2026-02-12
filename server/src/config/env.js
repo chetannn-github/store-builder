4c27f811c4abcb75dcdf9b727eebff821b109532
chetannn-github <chetan.rajawat25@gmail.com>
2026-02-12 14:19:52 +0530

    feat - make protocol variable

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -3,3 +3,4 @@ export const MONGO_URI = process.env.MONGO_URI;
 export const JWT_SECRET=process.env.JWT_SECRET;
 export const BASE_DOMAIN =  process.env.BASE_DOMAIN || "localhost";
 export const NODE_ENV = process.env.NODE_ENV || "development"
+export const PROTOCOL = NODE_ENV ===  "development" ? "http://" : "https://";
